29da118e0a37fcd582010060fca688e876270b30
Rob Ranf <rob@emiyaconsulting.com>
2024-10-01 17:47:19 -0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Rob Ranf on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashzillaTests/FlashzillaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaTests.swift
+//  FlashzillaTests
+//
+//  Created by Rob Ranf on 10/1/24.
+//
+
+import Testing
+@testable import Flashzilla
+
+struct FlashzillaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
